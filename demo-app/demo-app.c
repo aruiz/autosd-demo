e2e174dd352fc89707592288f4a6f589625567d6
Alberto Ruiz <aruiz@redhat.com>
2024-02-28 19:24:33 +0100

    fullscreen mode

--- demo-app/demo-app.c
+++ demo-app/demo-app.c
@@ -9,8 +9,8 @@ activate (GtkApplication *app,
     GtkWidget *button;
 
     window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_window_set_title (GTK_WINDOW (window), "AutoSD Demo");
+    gtk_window_fullscreen (GTK_WINDOW (window));
 
     button = gtk_button_new_with_label ("Hello World!");
     gtk_window_set_child (GTK_WINDOW (window), button);
